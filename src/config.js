e2c9e71c769f84310b026e16ffdfe7154b06683d
OvinduArchanaNambukara <48377535+OvinduArchanaNambukara@users.noreply.github.com>
2022-07-04 01:04:48 +0530

    set grapql api

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ export const config = {
     MONGO_USRNAME: 'medec_uni',
     MONGO_PWD: '65qCtRIvUfGQcfWr',
     MONGO_CLUSTER: 'medec',
-    MONGO_DB_NAME: 'MEDEC_DB'
+    MONGO_DB_NAME: 'MEDEC_DB',
+    SERVER_PORT: 4000,
+    ENVIRONMENT: 'development' // set production when build the project
 }
