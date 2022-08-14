2ea45e00af8708daed46977aaefcc20d48fc8003
ovindu_archana <ovindunambukara@gmail.com>
2022-08-14 16:19:45 +0530

    create token validation

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ export const config = {
     MONGO_DB_NAME: 'MEDEC_DB',
     SERVER_PORT: 4000,
     ENVIRONMENT: 'development', // set production when build the project
-    JWT_SECRET_ID: 'CODE_PIRATEZ'
+    JWT_SECRET_ID: 'CODE_PIRATEZ',
+    TOKEN_EXP_TIME: '12h'
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,12 +3,19 @@ import {config} from "../config.js";
 
 const createToken = (data) => {
     return jwt.sign(data, config.JWT_SECRET_ID, {
-        expiresIn: '12h'
+        expiresIn: config.TOKEN_EXP_TIME
     });
 };
 
-const verifyToken = (token) => {
-    return jwt.verify(token, config.JWT_SECRET_ID);
+const verifyToken = (token, tknInvalidCallback, tknValidCallback) => {
+    return jwt.verify(token, config.JWT_SECRET_ID, (error, decode) => {
+        if (error && typeof tknInvalidCallback === 'function') {
+            tknInvalidCallback();
+        } else {
+            if (typeof tknValidCallback === 'function')
+            tknValidCallback(decode);
+        }
+    });
 };
 
 const utils = {
